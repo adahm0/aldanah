add5e45a1076c06caa76fbf56b4830c724db8b74
adahm0 <danah056524@gmail.com>
2024-11-25 12:39:50 +0300

    hi

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+public class student{
+public static void Main(){
+    //1
+   /*? int stu;
+    string name = "aldanh";
+    int age = 18;
+    string grade ="100";
+   int.Parse(display(name+age+ grade));/*/
+    
+//2
+ /*/int[] array=new int[5];
+
+ int score=10;
+ int score2=20;
+ int score3=30;
+ int score4 =40;
+ int score5 =50;
+Console.WriteLine(array);/*/
+ //3
+int user=int.Parse(Console.ReadLine());
+int nm0=user*2;
+Console.WriteLine(nm0);
+
+ //4
+/*/Console.ReadLine();
+int number1= 10;
+if{
+    (number1> -1 )
+    Console.WriteLine("try agein");
+}
+else if{
+number1<1
+Console.WriteLine("sorry");
+}
+else if {
+    number1<0
+    Console.WriteLine("not work ,try agein");
+}
+else{
+    Console.WriteLine("hello");
+}
+
+//5
+string nameA = "ALDANAH";
+int ageA = 18;
+double wage = 20;
+Console.WriteLine("Hi my name"+ nameA+"Iam"+ageA+"yeras old and earn$"+wage) ;/*/
+
+    }
+    
+}
+ 
